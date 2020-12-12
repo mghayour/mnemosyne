497d23a5fb4f74310b4c08603f53c4b39ce550a6
Mahdi Ghayour <me@mghayour.ir>
2020-12-13 00:31:08 +0330

    support array and more logs

--- deepcopy.go
+++ deepcopy.go
@@ -18,6 +18,9 @@ func ShallowCopy(src interface{}, dst interface{}) {
 	if dstv.Kind() == Ptr {
 		dstv = dstv.Elem()
 	}
+	if dstv.Kind() == Ptr {
+		dstv = dstv.Elem()
+	}
 	if srcv.Kind() == Ptr {
 		srcv = srcv.Elem()
 	}
@@ -35,15 +38,25 @@ func ShallowCopy(src interface{}, dst interface{}) {
 	// Copy the elements
 	// maybe we need to support array, slice later.
 	switch srcv.Kind() {
+	case Array, Slice:
+		for i := 0; i < srcv.Len(); i++ {
+			dstv.Index(i).Set(srcv.Index(i))
+		}
+		return
 	case Map:
 		for _, k := range srcv.MapKeys() {
 			dstv.SetMapIndex(k, srcv.MapIndex(k))
 		}
+		return
 	case Struct:
 		for i, n := 0, srcv.NumField(); i < n; i++ {
 			if dstv.Field(i).CanSet() {
 				dstv.Field(i).Set(srcv.Field(i))
 			}
 		}
+		return
 	}
+
+	logrus.Errorf("Unsupported kind, %v, %v", srcv.Kind(), dstv.Kind())
+	debug.PrintStack()
 }
